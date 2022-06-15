6e33d7cc4c1ddd600d70850c881382ccd482d4c4
Ngahine Wakefield <28144736+ngas-wakefield@users.noreply.github.com>
2022-06-16 11:18:57 +1200

    can add, strike, remove todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
-
+import { Todo } from './Todo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
-  todoArray=[] 
+ todos : Todo[] = [];
+ newTodo : string;
+
+ saveTodo() {
+  if(this.newTodo){
+    let todo = new Todo();
+    todo.name = this.newTodo;
+    todo.isCompleted = true
+    this.todos.push(todo);
+    this.newTodo = ''
+   }else{
+    alert("Please Enter Todo");
+  }
+ }
+
+    done(id:number){
+      this.todos[id].isCompleted = !this.todos[id].isCompleted;
+    }
+
+    remove(id:number){
+      this.todos = this.todos.filter((v,i)=> i !== id)
+    }
+
 }
